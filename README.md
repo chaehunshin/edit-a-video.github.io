919bf0b0cb7bfae708a91de01e1852e96a793721
chaehun <33506478+chaehunshin@users.noreply.github.com>
2023-03-14 16:21:46 +0900

    Add files via upload

---
+++ README.md
@@ -0,0 +1,2 @@
+# Edit-A-Video-github-io
+Edit-A-Video features.
---
+++ sm/assets/teaser-data.js
@@ -0,0 +1,27 @@
+
+file_paths = {
+    "cow-walking-bull": "./sm/assets/our_results/a cow is walking/a bull is walking.gif",
+    "cow-walking-zebra": "./sm/assets/our_results/a cow is walking/a zebra is walking.gif",
+    "cow-walking-desert": "./sm/assets/our_results/a cow is walking/a cow is walking on the desert.gif",
+    "cow-walking-snow": "./sm/assets/our_results/a cow is walking/a cow is walking on the snow.gif",
+
+    "man-guitar-bear": "./sm/assets/our_results/a man is playing a guitar/a bear is playing a guitar.gif",
+    "man-guitar-monkey": "./sm/assets/our_results/a man is playing a guitar/a monkey is playing a guitar.gif",
+    "man-guitar-cartoon": "./sm/assets/our_results/a man is playing a guitar/a man is playing a guitar, cartoon style.gif",
+    "man-guitar-Matisse": "./sm/assets/our_results/a man is playing a guitar/a man is playing a guitar, Matisse style.gif",
+
+    "woman-swing-man": "./sm/assets/our_results/a woman is on the swing/a man is on the swing.gif",
+    "woman-swing-ironman": "./sm/assets/our_results/a woman is on the swing/a Iron Man is on the swing.gif",
+    "woman-swing-vangogh": "./sm/assets/our_results/a woman is on the swing/a woman is on the swing, Van Gogh style.gif",
+    "woman-swing-watercolor": "./sm/assets/our_results/a woman is on the swing/a watercolor painting that a woman is on the swing.gif",
+
+    "man-basketball-messi": "./sm/assets/our_results/a man is dribbling a basketball/a Lionel Messi is dribbling a basketball.gif",
+    "man-basketball-racoon": "./sm/assets/our_results/a man is dribbling a basketball/a racoon is dribbling a basketball.gif",
+    "man-basketball-oil": "./sm/assets/our_results/a man is dribbling a basketball/a oil painting that a man is dribbling a basketball.gif",
+    "man-basketball-renoir": "./sm/assets/our_results/a man is dribbling a basketball/a man is dribbling a basketball, Renoir style.gif",
+
+    "man-running-zombie": "./sm/assets/our_results/a man is running/a zombie is running.gif",
+    "man-running-werewolf": "./sm/assets/our_results/a man is running/a werewolf is running.gif",
+    "man-running-pixar": "./sm/assets/our_results/a man is running/a man is running, pixar style.gif",
+    "man-running-sketch": "./sm/assets/our_results/a man is running/a pencil sketch that a man is running.gif",
+}
